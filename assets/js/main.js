1f3605cea381eed4df3b079cdc17cf273f94c191
Davi Gomes <daviszgomes@gmail.com>
2023-08-17 00:35:24 -0300

    Added fuctions for Altura and Peso invalido

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,35 @@ body.addEventListener('onClick', function(){
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  console.log("Evento enviado");
-  setResultado('Ola mundo');
+  const inputPeso = e.target.querySelector('#peso');
+  const inputAltura = e.target.querySelector('#altura');
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+  console.log(peso,altura);
+
+  if (!peso){
+    setResultado('Peso inválido', false);
+    return;
+  }
+  if (!altura){
+    setResultado('Altura inválida', false);
+    return;
+  }
+
+
+
 });
 
-function setResultado(msg) {
+
+function createP () {
+  const p = document.createElement('p');
+  return p;
+}
+
+
+function setResultado(msg, isValid) {
    const resultado = document.querySelector('.resultado');
-   resultado.innerHTML = msg;
+   resultado.innerHTML = '';
+   const p = criaP();
 }
